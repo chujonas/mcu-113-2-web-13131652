f976e3b7a919e66bb3bdb0919bb77c14eeebd6f2
jonas <13131652@me.mcu.edu.tw>
2025-04-29 19:10:23 +0800

    feat(product): 建立產品資料模型

---
+++ src/app/models/product.ts
@@ -0,0 +1,22 @@
+export class Product {
+  constructor(initDate?: Partial<Product>) {
+    if (!initDate) return;
+    Object.assign(this, initDate);
+  }
+
+  id!: number;
+
+  name!: string;
+
+  company!: string;
+
+  authors!: string;
+
+  price!: number;
+
+  isShow!: boolean;
+
+  photoUrl!: string;
+
+  createDate!: Date;
+}
